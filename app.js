856869ac6bd3e713d10aa47bde77fff456c27e94
BernardoHer <bernaher19@gmail.com>
2022-10-10 17:32:02 -0500

    fix

---
+++ app.js
@@ -0,0 +1,10 @@
+const inputInf = document.querySelectorAll('.input-inf');
+const submitBtn = document.getElementById('claimBtn');
+const form = document.getElementById('form');
+
+
+console.log(inputInf);
+console.log(submitBtn);
+console.log(form);
+
+
