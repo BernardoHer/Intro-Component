73522de100b2b4c01589b9c751dd960fc3bd4cc8
BernardoHer <bernaher19@gmail.com>
2022-10-11 14:07:42 -0500

    js and fix css

--- app.js
+++ app.js
@@ -2,9 +2,43 @@ const inputInf = document.querySelectorAll('.input-inf');
 const submitBtn = document.getElementById('claimBtn');
 const form = document.getElementById('form');
 
+const checkValidity =()=>{
+    const inputFirstName = document.getElementById('input-first-name');
+    const inputLastName = document.getElementById('input-last-name');
+    const inputEmail = document.getElementById('input-email');
+    const inputPassword = document.getElementById('input-password');
 
-console.log(inputInf);
-console.log(submitBtn);
-console.log(form);
+    const firstNameValue = inputFirstName.value.trim();
+    const lastNameValue = inputLastName.value.trim();
+    const emailValue = inputEmail.value.trim();
+    const passwordValue = inputPassword.value.trim();
 
+    const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+    if(firstNameValue ===''){
+        inputInf[0].classList.add('error');
+    } else {
+        inputInf[0].classList.remove('error');
+    }
+    if(lastNameValue ===''){
+        inputInf[1].classList.add('error');
+    } else {
+        inputInf[1].classList.remove('error');
+    }
+    if(emailValue.match(regEx)){
+        inputInf[2].classList.remove('error');
+    } else {
+        inputInf[2].classList.add('error');
+    }
+    if(passwordValue ===''){
+        inputInf[3].classList.add('error');
+    } else {
+        inputInf[3].classList.remove('error');
+    }
+
+};
+
+submitBtn.addEventListener('click',(even)=>{
+    even.preventDefault();
+    checkValidity();
+})
